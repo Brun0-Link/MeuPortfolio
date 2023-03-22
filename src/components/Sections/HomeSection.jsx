e75b1b6b137d837fff8697b269cebc6729fc4701
Bruno Alves <brunoalvestrajano250@gmail.com>
2023-03-22 16:33:21 -0300

    first mobile adaptation

--- src/components/Sections/HomeSection.jsx
+++ src/components/Sections/HomeSection.jsx
@@ -2,7 +2,7 @@ export function HomeSection() {
   return (
     <section className="home" id="home">
       <div className="profile">
-        <img className="profile__foto" src="" alt="" />
+        {/* <img className="profile__foto" src="" alt="" /> */}
         <div>
           <span className="profile__text">Olá, meu nome é</span>
           <h1 className="profile__name">Bruno Trajano</h1>
